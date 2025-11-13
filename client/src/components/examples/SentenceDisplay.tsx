c6332a6fb579ec2805c7fc8cea05de78a4311add
anilujohn <anil.john13@gmail.com>
2025-11-13 13:58:49 +0530

    Add client/src/components/examples/SentenceDisplay.tsx

---
+++ client/src/components/examples/SentenceDisplay.tsx
@@ -0,0 +1,11 @@
+import SentenceDisplay from '../SentenceDisplay';
+
+export default function SentenceDisplayExample() {
+  return (
+    <SentenceDisplay
+      language="hindi"
+      originalScript="नमस्ते, आप कैसे हैं?"
+      transliteration="Namaste, aap kaise hain?"
+    />
+  );
+}
